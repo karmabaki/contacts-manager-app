143a1b7c555cf310687d199f01028323f4be0fe5
Sasan <mahdiesmaeili483@gmail.com>
2026-05-03 22:57:34 +0330

    spinner fixed

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 const Spinner = () => {
   return (
     <div className="flex justify-center items-center py-10">
-      <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+      <div className="animate-spin rounded-full h-12 w-12 border-4 border-blue-500 border-t-transparent"></div>
     </div>
   );
 };
